ef095f95bc84c8601610198d85ac8e37b5aa5782
dziarskihank <dziarskihank@users.noreply.github.com>
2020-06-18 12:09:53 +0200

    Zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,5 +1,6 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TestingMain {
@@ -9,9 +10,31 @@ public class TestingMain {
         String result = simpleUser.getUsername();
 
         if (result.equals("theForumUser")){
-            System.out.println("test OK");
+            System.out.println("test theForumUser jest OK");
         } else {
-            System.out.println("Error!");
+            System.out.println("Error - test theForumUser !");
+        }
+
+        Calculator testPair = new Calculator(15, 25);
+        int adding = testPair.add();
+        int substracting = testPair.substract();
+
+        if (adding == 40)
+        {
+            System.out.println("Test dodawania jest OK");
+        }
+        else
+        {
+            System.out.println("Dodawanie Error");
+        }
+
+        if (substracting == -10)
+        {
+            System.out.println("Test odejmowania jest OK");
+        }
+        else
+        {
+            System.out.println("Odejmowanie error");
         }
     }
 }
