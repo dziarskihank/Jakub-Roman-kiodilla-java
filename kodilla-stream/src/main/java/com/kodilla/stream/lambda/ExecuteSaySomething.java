ab99edb7f32fb1e86240d2bdcb10930bf6a27919
dziarskihank <dziarskihank@users.noreply.github.com>
2020-06-22 18:02:00 +0200

    Zadanie 7.1

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/lambda/ExecuteSaySomething.java
@@ -0,0 +1,11 @@
+//package com.kodilla.stream.lambda;
+//
+//import java.util.concurrent.Executor;
+//
+//public class ExecuteSaySomething implements Executor {
+//
+//   @Override
+//    public void process() {
+//       System.out.println("This is an example text.");
+//    }
+//}
